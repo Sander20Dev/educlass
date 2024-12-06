a77fa5bbdd35d12245ebacaa992dea537f9dac6f
Sander <sander20dev@outlook.com>
2024-12-05 23:11:54 -0500

    deploy error PageProps

--- app/(logged)/class/[classId]/edit/page.tsx
+++ app/(logged)/class/[classId]/edit/page.tsx
@@ -1,13 +1,16 @@
 import { getClass } from '@/app/lib/db/server'
 import LoggedPageTemplate from '@/app/lib/logged-page/template'
+import { PageProps } from '@/app/lib/types/types'
 import { setupUser } from '@/app/lib/utils/setup-user'
 import ClassEditor from '@/app/ui/class/class-editor'
 import ForbiddenPage from '@/app/ui/general/forbidden-page'
 import { notFound, redirect } from 'next/navigation'
 
 export default async function ClassPage({
-  params: { classId },
-}: Readonly<{ params: { classId: string } }>) {
+  params,
+}: PageProps<{ classId: string }>) {
+  const { classId } = await params
+
   const { rawUser, user } = await setupUser()
 
   if (!rawUser) return redirect('/login')
---
+++ app/lib/types/types.ts
@@ -0,0 +1,3 @@
+export type PageProps<T = unknown> = {
+  params: Promise<T>
+}
